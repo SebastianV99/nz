83b0c25ae735988ba757d1ab65171870f53423f3
SebastianV99 <vollmer.sebastian@outlook.de>
2023-03-30 15:46:52 +0200

    objekt erstellt

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
-var map = L.map('map').setView([-44.7, 169.15], 13);
+let stop_lat = -44.7;
+let stop_lng = 169.15;
+
+
+let map = L.map('map').setView(stop_lat, stop_lng, 13);
 
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
 
-        L.marker([-44.7, 169.15]).addTo(map)
+        L.marker([stop_lat,stop_lng]).addTo(map)
             .bindPopup('Wanaka')
             .openPopup();

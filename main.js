3eaaba8171f27a3944ad36bd9b2ab5303f4a2c69
SebastianV99 <vollmer.sebastian@outlook.de>
2023-03-30 15:50:56 +0200

    variablen title und zoom

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
-let stop_lat = -44.7;
-let stop_lng = 169.15;
+let stop_lat = -44.71;
+let stop_lng = 169.16;
+let zoom = 13;
+let title = 'Wanaka';
 
 
-let map = L.map('map').setView(stop_lat, stop_lng, 13);
+let map = L.map('map').setView(stop_lat, stop_lng, zoom);
 
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
 
         L.marker([stop_lat,stop_lng]).addTo(map)
-            .bindPopup('Wanaka')
+            .bindPopup(title)
             .openPopup();
